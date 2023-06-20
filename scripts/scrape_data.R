3d6d4f6b8546f9586bbf04c80b510917d8d533de
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2023-06-20 15:27:33 +0200

    Update scrape_data.R

--- scripts/scrape_data.R
+++ scripts/scrape_data.R
@@ -20,17 +20,13 @@ rm(pkg)
 setwd("~/Desktop/Prueba_202313/")
 
 
+
 readHTML<-function(page_numb){
-  x<-read_html(paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_",page_numb,".html")) 
-  #Faltan Pasos para obtener la tabla (completar ustedes)
+  x<-read_html(paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_",page_numb,".html")) %>% html_table()
+  x<-x[[1]]            
 }
 
 db_list<-lapply(1:10,readHTML)  #itera sobre las páginas y retorna una lista con 10 elementos
 db<-do.call(rbind,db_list) #une todo en un data.frame
 
-# Seguimos con el codigo _ DR
 
-modelo2caret_loocv <- train(lnw_2016~educ, 
-                           data = nlsy, 
-                            method = 'lm',
-                             trControl= ctrl_loocv )
